ae84b580ea4a6086720c087437912fb0385935c2
Jaydaar <jaydaar@gmail.com>
2018-01-12 18:29:38 -0400

    Added classes to show inheritance polymorphism, and aggregation.

--- game/main.py
+++ game/main.py
@@ -1,26 +1,7 @@
-from player import Player
+a = 3
+b = "kevin"
+c = 1, 2, 3
 
-kevin = Player("Kevin")
-
-print(kevin.name)
-print(kevin.lives)
-kevin.lives -= 1
-print(kevin)
-kevin.lives -= 1
-print(kevin)
-kevin.lives -= 1
-print(kevin)
-kevin.lives -= 1
-print(kevin)
-
-kevin.level = 2
-print(kevin)
-
-kevin.level += 5
-print(kevin)
-
-kevin.level = 3
-print(kevin)
-
-kevin.score = 500
-print(kevin)
+print(a)
+print(b)
+print(c)
